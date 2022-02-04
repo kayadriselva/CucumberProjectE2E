db2e2178c5c6f4530a1b5c5eeacfa8f4239a3e5f
Kayadriselva <kayadri.selva12@gmail.com>
2022-02-05 00:06:00 +0530

    co

--- src/test/java/org/qa/amazon/runnerclass/RunnerFile.java
+++ src/test/java/org/qa/amazon/runnerclass/RunnerFile.java
@@ -11,9 +11,9 @@ import io.cucumber.junit.CucumberOptions;
 		glue = {"org.qa.amazon.testcases", "org.qa.amazon.hooks"},
 		monochrome=true,
 		dryRun=false,
-		plugin= {"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+		plugin= {"pretty", "html:target/cucumber-report/cucumber.html"}
 		)
-		
+		//"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
 public class RunnerFile {
 
 }
